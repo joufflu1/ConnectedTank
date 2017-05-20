6d34b6de764c1f05928f20bb840fc7db0b7dd9ab
Odulbaf <lfablet@gmail.com>
2017-05-21 00:35:38 +0200

    Add OpenWeatherMap Add Angular to parse OpenWeatherMap API

---
+++ static/weather.js
@@ -0,0 +1,43 @@
+var myApp = angular.module('myApp', ['ui.bootstrap']);
+
+myApp.controller('MainCtrl', ['$scope', '$http', 'myService', '$interval', function ($scope, $http, myService, $interval) {
+    $scope.text = 'Il pleut ? ';
+    $scope.showAlert = null;
+    $scope.alertType = "danger";
+    $scope.alertMessage = "Alert: Des precipitation sont à prevoir. Il est recommandé de vider votre Cuve";
+
+    myService.async("2971117").then(function (d) {
+        $scope.rain = d + " %";
+    });
+
+    // Alert: Des precipitation sont a prevoir. Il est recommandé de vider votre Cuve
+
+    $interval(function () {
+        $scope.rain = "100 %";
+        $scope.showAlert = true;
+
+        // Refresh every 10 second
+    }, 10000);
+
+    $scope.removeAlert=function(){
+        $scope.showAlert = false;
+    }
+
+}]);
+
+myApp.factory('myService', function ($http) {
+    var myService = {
+        async: function (city) {
+            return $http({
+                method: 'GET',
+                url: "http://api.openweathermap.org/data/2.5/forecast/daily?id=" + city + "&cnt=5&appid=ccc8240263784ad9f41414174c20d05a"
+            })
+                .then(function successCallback(response) {
+                    return response.data.list[0].humidity;
+                }, function errorCallback(response) {
+                    return response.cod;
+                });
+        }
+    };
+    return myService;
+});
